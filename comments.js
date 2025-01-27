d5ce4994da55fd77204af08bc23ff82b59a443fb
karunakar999-198 <karunakarreddy17@gmail.com>
2025-01-27 01:56:41 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,18 @@
 //create web server
-const express = require('express');
-const app = express();
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
-//import comments.js
-const comments = require('./comments.js');
-
-//use comments.js
-app.use('/comments', comments);
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
