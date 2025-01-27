4618a54be71d842b4bb6ecd30b583a8b6e89e50c
karunakar999-198 <karunakarreddy17@gmail.com>
2025-01-27 01:59:11 +0000

    copilot fourth commit

--- comments.js
+++ comments.js
@@ -1,8 +1,7 @@
-//create web server
+// Create web server
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
